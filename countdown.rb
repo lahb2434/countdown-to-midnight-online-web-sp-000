a0ada63bb8ad845a22a23567246c0fdfe0a71f98
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-23 18:14:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,15 +1,23 @@
 #write your code here
 number = 10
+<<<<<<< HEAD
 digit = 10
 def countdown(number)
   while number > 0 
     puts "#{number} SECOND(S)!"
+=======
+def countdown(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    countdown_with_sleep
+>>>>>>> f4059eb2e24fd676262878e0a16caefba37df4dd
     number -= 1
   end
   return "HAPPY NEW YEAR!"
 end
 
 
+<<<<<<< HEAD
 def countdown_with_sleep(number)
   while number > 0 
     puts "#{number} SECOND(S)!"
@@ -17,6 +25,10 @@ def countdown_with_sleep(number)
     number -= 1
   end
   return "HAPPY NEW YEAR!"
+=======
+def countdown_with_sleep
+     sleep (1)
+>>>>>>> f4059eb2e24fd676262878e0a16caefba37df4dd
 end
 
 countdown(number)
